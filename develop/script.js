4aec400d144a168353439c29c6205745fb1e39e3
v1ct0r14m <tori.a.g.mota@gmail.com>
2022-10-20 23:57:54 -0500

    working on code for prompts

--- develop/script.js
+++ develop/script.js
@@ -1,27 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-/* client wants to use following: pwdCriteria
-
-define variables // OR MAKE ARRAYS -->variables
- lowercase, --> abcdefghijklmnopqrstuvwxyz
- uppercase, --> ABCDEFGHIJKLMNOPQRSTUVWXYZ
-  numeric, --> function getRandomInt(min, max) {
-    min = 0
-    max = min
-  }
-  special characters --> ~!@#$%^&*-=+_.?
-
-*/
-
-/* pwd must be at least 8 characters and no more than 128 characters --> MOVED TO GENERATEPWD
-
-  while loop for pwdLength -->
-  prompt for pwdLength-->for length input
-  if statement for pwdLength-->user input for pwdCriteria
-
-
-*/
+// client wants to use following: pwdCriteria
+var lower = 'abcdefghijklmnopqrstuvwxyz';
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var special =  '~!@#$%^&*-=+_.?';
+var randomNum = Math.floor(Math.random);
 
 // Write password to the #password input
 function writePassword() {
@@ -39,9 +23,46 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //to carry out operations for password generator
-generatePassword();
+function generatePassword() {
+
+  pwdLength = 0;
+  lower = '';
+  upper = '';
+  special = '';
+
+  while (pwdLength > 8 || pwdLength < 128) {
+    pwdLength = prompt('how many characters would you like your password to be? (password must be at least 8 characters)');
+
+    if (pwdLength < 8 || pwdLength > 128) {
+
+      alert('password does not meet length criteria');
+      
+      return generatePassword;
+    }
+    else {
+      
+      return true;
+    }
+  
+  
+  
+  }
+
+}
+
+
 
 ////while loop here --> while characters are between 8 - 128
 //// pwdLength, #, symbols, upper n lower case letters decided by user input here
 //////prompts for each 
 //////all prompts to be answerable
+
+
+/* pwd must be at least 8 characters and no more than 128 characters --> MOVED TO GENERATEPWD
+
+  while loop for pwdLength -->
+  prompt for pwdLength-->for length input
+  if statement for pwdLength-->user input for pwdCriteria
+
+
+*/
